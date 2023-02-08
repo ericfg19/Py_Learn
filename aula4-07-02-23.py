27689787b69f278834cb39dfbc0816cd24b3c81f
EricFG <eric_fg@live.com.ar>
2023-02-07 22:36:37 -0300

    Create aula4-07-02-23.py

---
+++ aula4-07-02-23.py
@@ -0,0 +1,14 @@
+#calcular altura de um cone, com 3 variáveis, sendo uma 3.14 (pi)
+pi = 3.14
+r = 4
+h = 5
+
+volume = (pi * (r**2) * h) / 3
+
+print(volume)
+
+#calcular uma conversão de celsius para farenheit
+c = 10
+f = c * 9/5 + 32
+
+print(f)
