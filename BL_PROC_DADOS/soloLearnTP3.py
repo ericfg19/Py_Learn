baf54fb126c4a6e2c5aa57d5b6ce013fa9a1e9bc
EricFG <eric_fg@live.com.ar>
2023-03-13 20:17:59 -0300

    Create soloLearnTP3.py

---
+++ BL_PROC_DADOS/soloLearnTP3.py
@@ -0,0 +1,14 @@
+print(6**2)
+print(8 ** (1 / 3))
+print(10 // 4)
+
+print(7 % (5 // 2))
+print(6 // 1.5)
+print(1 + 4*3)
+print( (3**2)//2 )
+
+bill = int(input())
+x=20
+y=100
+tip=(bill*int(x)/int(y))
+print(float(tip))
